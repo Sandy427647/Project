4b4949c11f3b5e074a9ff1b848ad3a8f121bab94
Sun Deep <forgetyou6666@gmail.com>
2024-11-05 11:51:24 +0545

    Project updated sucessfully

--- demo/src/main/java/com/vlog/demo/DemoApplication.java
+++ demo/src/main/java/com/vlog/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("hello git");
 		System.out.println("thi9s is");
+		System.out.println("thi9s ars");
 	}
 	
 }
